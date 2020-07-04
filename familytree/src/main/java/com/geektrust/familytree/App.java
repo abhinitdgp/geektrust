8dc85ae52a501bebd9c7d9299aecb733f705e914
Abhishek Aggarwal <abhishek.aggarwal02.93@gmail.com>
2020-07-05 00:50:22 +0530

    Initial commit

---
+++ familytree/src/main/java/com/geektrust/familytree/App.java
@@ -0,0 +1,13 @@
+package com.geektrust.familytree;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
